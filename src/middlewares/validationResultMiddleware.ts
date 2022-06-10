ff0b6f5ad64f36da6754d87bdab85d87781d1048
Alex <bbqsauce108@gmail.com>
2022-06-10 21:39:48 +0300

    fix error messages field2

--- src/middlewares/validationResultMiddleware.ts
+++ src/middlewares/validationResultMiddleware.ts
@@ -2,7 +2,15 @@ import {NextFunction, Request, Response} from "express";
 import {validationResult} from "express-validator";
 
 export const validationResultMiddleware = (req: Request, res: Response, next: NextFunction) =>{
-    const errors = validationResult(req)
+    const myValidationResult = validationResult.withDefaults({
+        formatter: error => {
+            return {
+                message: error.msg,
+                field: error.param
+            }
+        }
+    })
+    const errors = myValidationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({errorsMessages: errors.array()})
     }else{
