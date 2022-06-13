b679d66dc96b1bc94ca8e4299614c64f7ea09615
Alex <bbqsauce108@gmail.com>
2022-06-13 13:11:48 +0300

    try black list

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,18 @@ import express, {NextFunction, Request, Response} from 'express'
 import cors from 'cors'
 import bodyParser from "body-parser";
 import {videosRouter} from "./routes/videos-router";
+import {blackListMiddleware} from "./middlewares/black-list-middleware";
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 const port = process.env.PORT || 5000
 
-app.use( (req:Request, res: Response, next: NextFunction)=>{
-    const ip = req.ip
-    // console.log(ip)
-    next()
-})
+app.set('trust proxy', true)
+app.use( blackListMiddleware)
 
 app.get('/', (req:Request, res: Response) => {
-    const ip = req.ip
-    res.send('Videos backend! Ypur ip: ' + ip )
+    const ip = req.socket.localAddress
+    res.send('Videos backend! Your ip: ' + ip )
 })
 
 app.use('/videos', videosRouter)
---
+++ src/middlewares/black-list-middleware.ts
@@ -0,0 +1,17 @@
+import {NextFunction, Request, Response} from "express";
+const blackList = [
+    {
+        ip: '176.14.144.0'
+    }
+]
+export const blackListMiddleware = (req: Request, res: Response, next: NextFunction) =>{
+    console.log('Black list middleware')
+    const blockedId = blackList.find( a => a.ip === req.ip)
+    if(blockedId){
+        res.status(400).send('You are in a black list')
+        return
+    }else{
+        next()
+    }
+
+}
